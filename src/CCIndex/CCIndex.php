d72409e9e8199b61da04f06f5d8c63f35b0f6bec
Daniel <Svenssonpsycho@hotmail.com>
2013-04-15 15:34:42 +0200

    update 0,1

---
+++ src/CCIndex/CCIndex.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Standard controller layout.
+*
+* @package SkolaCore
+*/
+class CCIndex implements IController {
+
+  /**
+* Implementing interface IController. All controllers must have an index action.
+*/
+  public function Index() {
+    global $sk;
+    $sk->data['title'] = "The Index Controller";
+    $sk->data['main'] = "<h1>The Index Controller</h1>";
+  }
+}
